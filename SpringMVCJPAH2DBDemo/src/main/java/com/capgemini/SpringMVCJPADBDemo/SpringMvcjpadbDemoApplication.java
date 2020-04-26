3b19a3a031d0b41b2f47ae22cd04f1f453f07677
sapzende <54667589+sapzende@users.noreply.github.com>
2020-04-26 22:09:03 +0530

    Add files via upload

---
+++ SpringMVCJPAH2DBDemo/src/main/java/com/capgemini/SpringMVCJPADBDemo/SpringMvcjpadbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.SpringMVCJPADBDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcjpadbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcjpadbDemoApplication.class, args);
+	}
+
+}
